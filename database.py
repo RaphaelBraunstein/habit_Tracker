f8ed1183ed1bdbab492464534f36009901a011e8
Raphael Braunstein <raphael.braunstein@gmx.at>
2023-03-12 20:42:06 +0100

    completed database.py with documentation

--- database.py
+++ database.py
@@ -1,14 +1,20 @@
 import sqlite3
+import habit as hb
 
-import habit
+"""
+When running the file database.py it creates the database.
+That's why the connect-method, the cursor-method and the 2 table executes are outside of a method.
+When running the file, it will create 2 tables:
+habits: habits represent the habits.py class itself with all attributes. The NAME_ attribute is the primary key,
+because it is very unlikely to have 2 habits with the same name, so i chose the name to be the primary key.
 
+dates: the dates table holds the dates of the tasks when a habit has been completed.
+For each date, it holds the date itself,
+the calendar week for weekly habits and the name of the habit as a foreign key,
+so the date can be assigned to the habit. 
+"""
 conn = sqlite3.connect("habit.db")
-
 c = conn.cursor()
-
-conn.execute("DROP TABLE IF EXISTS habits")
-conn.execute("DROP TABLE IF EXISTS dates")
-
 c.execute("""CREATE TABLE IF NOT EXISTS habits
                 ( 
                 NAME_ TEXT PRIMARY KEY NOT NULL,
@@ -29,17 +35,31 @@ c.execute("""CREATE TABLE IF NOT EXISTS dates
 
 
 def insert_habit_into_db(habit):
+    """
+    This method takes a given habit and stores it in the database
+    :param habit: Habit which was created by the user
+    """
     conn.execute("""INSERT INTO habits VALUES (?,?,?,?,?,?,?)""",
                  (habit.name, habit.periodicity, habit.task_specification, habit.created_date,
                   habit.missed_periods_counter, habit.longest_streak, habit.current_streak))
     conn.commit()
 
 
-def check_off_task(date, calendarweek, habit_name, habit):
+def check_off_task(date, calendarweek, habit_name, missed_periods_counter, longest_streak, current_streak):
+    """
+    This method updates the habit's longest streak, current streak and missed counter.
+    After updating the counters of the habit, the method stores a new date into the dates table
+    :param date: The date on which the task has been checked.
+    :param calendarweek: The calendar week of the date above.
+    :param habit_name: The name of the habit, which should be checked off.
+    :param missed_periods_counter: Counts how many times someone missed a period
+    :param longest_streak: Is the longest streak how often someone checked a task in a row.
+    :param current_streak: Is the current counter on how many times someone managed to check a habit in a row.
+    """
     conn.execute("""UPDATE habits 
     SET MISSED_COUNTER = ?,
     LONGEST_STREAK = ?, CURRENT_STREAK = ? WHERE NAME_ = ?""",
-                 (habit.missed_periods_counter, habit.longest_streak, habit.current_streak, habit_name))
+                 (missed_periods_counter, longest_streak, current_streak, habit_name))
 
     conn.execute("""INSERT INTO dates VALUES(?, ?, ?, ?)""",
                  (None, date, calendarweek, habit_name))
@@ -47,14 +67,22 @@ def check_off_task(date, calendarweek, habit_name, habit):
 
 
 def delete_habit(habit_name):
+    """
+    This method deletes all dates that belong to a specific habit.
+    After deleting the dates, it deletes the habit itself
+    :param habit_name: Is the name of the habit that should be deleted.
+    """
     conn.execute("DELETE FROM dates WHERE habit_name = ?", (habit_name,))
     conn.execute("DELETE FROM habits WHERE NAME_ = ?", (habit_name,))
     conn.commit()
 
 
 def load_data():
-    """returns a list of all habits with all dates based on weekly or daily
-       habit part done so far, dates missing yet """
+    """
+    This method loads the whole database. First, it loads all habits into a list, then it loads the dates which belong the a habit
+    into the habit.completed_periods list.
+    :return: habits, a list of all habits with their dates.
+    """
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM habits")
     rows = cursor.fetchall()
@@ -63,7 +91,7 @@ def load_data():
 
     habits = []
     for row in rows:
-        habit_ = habit.Habit(*row)
+        habit_ = hb.Habit(*row)
         for date in rows_dates:
             if habit_.periodicity == "daily":
                 if date[3] == habit_.name:
