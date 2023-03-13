54d5c9ed5c76c554a351512aee80e1c3d4dbec28
Raphael Braunstein <raphael.braunstein@gmx.at>
2023-03-13 16:35:33 +0100

    modified

--- habitTracker_test.py
+++ habitTracker_test.py
@@ -1,6 +1,7 @@
 import datetime
 import unittest
 
+import database
 import habitTracker
 from habit import Habit
 
@@ -8,10 +9,12 @@ from habit import Habit
 class HabitTrackerTest(unittest.TestCase):
 
     def test_get_current_habits(self):
+        database.init_db()
         tracker = habitTracker.Tracker()
         self.assertEqual(tracker.get_current_habits(), tracker.habit_list)
 
     def test_create_habit(self):
+        database.init_db()
         tracker = habitTracker.Tracker()
         length = len(tracker.habit_list)
         tracker.create_habit("habit_1", "daily", "daily_habit_one")
@@ -19,12 +22,14 @@ class HabitTrackerTest(unittest.TestCase):
         self.assertEqual(length, length_after - 1)
 
     def test_check_off_habit_task(self):
+        database.init_db()
         tracker = habitTracker.Tracker()
         tracker.create_habit("habit_2", "daily", "daily_habit_one")
         tracker.check_off_habit_task("habit_2")
         self.assertEqual(tracker.habit_list[0].is_task_completed(), True)
 
     def test_delete_habit(self):
+        database.init_db()
         tracker = habitTracker.Tracker()
         tracker.create_habit("habit_3", "daily", "daily_habit_one")
         length = len(tracker.habit_list)
@@ -32,6 +37,7 @@ class HabitTrackerTest(unittest.TestCase):
         self.assertEqual(len(tracker.habit_list), length - 1)
 
     def test_get_same_periodicity(self):
+        database.init_db()
         tracker = habitTracker.Tracker()
         tracker.create_habit("habit_4", "daily", "daily_habit_one")
         tracker.create_habit("habit_5", "weekly", "daily_habit_one")
@@ -40,6 +46,7 @@ class HabitTrackerTest(unittest.TestCase):
         self.assertEqual(list_[0].periodicity, "weekly")
 
     def test_get_current_open(self):
+        database.init_db()
         tracker = habitTracker.Tracker()
         tracker.create_habit("habit_6", "daily", "daily_habit_one")
         tracker.create_habit("habit_7", "weekly", "daily_habit_one")
@@ -49,6 +56,7 @@ class HabitTrackerTest(unittest.TestCase):
         self.assertEqual(len(list_), 2)
 
     def test_get_current_done(self):
+        database.init_db()
         tracker = habitTracker.Tracker()
         tracker.create_habit("habit_9", "daily", "daily_habit_one")
         tracker.create_habit("habit_10", "weekly", "daily_habit_one")
